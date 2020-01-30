d51e2a5f210ee3f43aee3108c2c845bfcb8f4b50
DanielGG1618 <57752868+DanielGG1618@users.noreply.github.com>
2020-01-30 19:43:50 +0400

    Add files via upload

---
+++ Organizer/Organizer/Head.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Organizer
+{
+    public partial class Head : Form
+    {
+        bool developmentMode;
+        DateTime dateTime;
+        
+        public Head()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTime = DateTime.Today;
+            DateText.Text = dateTime.Day + "." + dateTime.Month.ToString("00") + "." + dateTime.Year;
+        }
+
+        private void DevelopModeButton_Click(object sender, EventArgs e)
+        {
+            InDevelop();
+        }
+
+        private void SaveScreenButton_Click(object sender, EventArgs e)
+        {
+            InDevelop();
+        }
+
+        private void LoadFromButton_Click(object sender, EventArgs e)
+        {
+            InDevelop();
+        }
+
+        private void SaveToButton_Click(object sender, EventArgs e)
+        {
+            InDevelop();
+        }
+
+        private void ChangesButton_Click(object sender, EventArgs e)
+        {
+            InDevelop();
+        } 
+
+        private void DateMinusButton_Click(object sender, EventArgs e)
+        {
+            dateTime = dateTime.AddDays(-1);
+            DateText.Text = dateTime.Day + "." + dateTime.Month.ToString("00") + "." + dateTime.Year;
+        }
+        
+        private void SettingsButton_Click(object sender, EventArgs e)
+        {
+            Settings settings = new Settings();
+            settings.Show();
+        }
+
+        private void InDevelop()
+        {
+            MessageBox.Show("Кнопка в разработке", "Эrrоr");
+        }
+
+        private void Title2_Click(object sender, EventArgs e)
+        {
+            TaskForm f = new TaskForm("Физика", "Проводники");
+            f.Show();
+        }
+
+        private void DatePlusButton_Click(object sender, EventArgs e)
+        {
+            dateTime = dateTime.AddDays(1);
+            DateText.Text = dateTime.Day + "." + dateTime.Month.ToString("00") + "." + dateTime.Year;
+        }
+    }
+}
---
+++ Organizer/Organizer/Settings.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Organizer
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.languageSelector = new System.Windows.Forms.ComboBox();
+            this.languagePict = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.languagePict)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // languageSelector
+            // 
+            this.languageSelector.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.languageSelector.BackColor = System.Drawing.SystemColors.Window;
+            this.languageSelector.Cursor = System.Windows.Forms.Cursors.Default;
+            this.languageSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.languageSelector.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.languageSelector.FormattingEnabled = true;
+            this.languageSelector.Items.AddRange(new object[] {
+            "Russian",
+            "Англиский"});
+            this.languageSelector.Location = new System.Drawing.Point(110, 39);
+            this.languageSelector.Margin = new System.Windows.Forms.Padding(6, 30, 30, 30);
+            this.languageSelector.Name = "languageSelector";
+            this.languageSelector.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.languageSelector.Size = new System.Drawing.Size(200, 24);
+            this.languageSelector.TabIndex = 0;
+            // 
+            // languagePict
+            // 
+            this.languagePict.Image = global::Organizer.Properties.Resources.rus;
+            this.languagePict.Location = new System.Drawing.Point(15, 12);
+            this.languagePict.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.languagePict.Name = "languagePict";
+            this.languagePict.Size = new System.Drawing.Size(83, 83);
+            this.languagePict.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.languagePict.TabIndex = 1;
+            this.languagePict.TabStop = false;
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(798, 453);
+            this.Controls.Add(this.languagePict);
+            this.Controls.Add(this.languageSelector);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(816, 500);
+            this.MinimumSize = new System.Drawing.Size(816, 500);
+            this.Name = "Settings";
+            this.Text = "Настройки";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.languagePict)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox languageSelector;
+        private System.Windows.Forms.PictureBox languagePict;
+    }
+}
